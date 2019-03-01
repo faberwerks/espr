9a32f6e5632523475ddad004431815da634181e0
Crouston <yosuam34@gmail.com>
2019-03-01 23:15:24 +0700

    Dialogue system added
    -Dialogue Manager added
    -Dialogue added
    -NPC&Choices added

---
+++ Goat/Assets/Script/DialogueManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    private Queue<string> sentences;
+    private Dialogue tempDialogue;
+    
+    public Text nameNPC;
+    public Text dialogueText;
+
+    private void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        tempDialogue = dialogue;
+        nameNPC.text = dialogue.nameNPC;
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentences();
+    }
+
+    public void DisplayNextSentences()
+    {
+        if(sentences.Count == 0)
+        {
+            foreach (GameObject choices in tempDialogue.choices)
+            {
+                choices.SetActive(true);
+            }
+            return;
+        }
+
+        
+        string sentence =  sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    public void DeleteChoices()
+    {
+
+        if (tempDialogue != null)
+        {
+            foreach (GameObject choices in tempDialogue.choices)
+            {
+                choices.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Goat/Assets/Script/NPC.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public void  TalkToNPC()
+    {
+        FindObjectOfType<DialogueManager>().DeleteChoices();
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
