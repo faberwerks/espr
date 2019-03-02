14560c87d538290d1b045399d347d0fa573b43f3
Crouston <yosuam34@gmail.com>
2019-03-02 20:23:49 +0700

    Attribute bar added

---
+++ Goat/Assets/Script/PlayerAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAttribute : MonoBehaviour
+{
+    public float hunger;
+    public float energy;
+    public float fun;
+    public float apathy;
+    public float social;
+
+    public float fullHunger;
+    public float fullEnergy;
+    public float fullFun;
+    public float fullApathy;
+
+    public Image hungerBar;
+
+    private void Start()
+    {
+        fullHunger = 100;
+        hunger = 80;
+    }
+
+    private void Update()
+    {
+        hungerBar.fillAmount = hunger / fullHunger;
+    }
+}
