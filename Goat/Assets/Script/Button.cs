969a7f48a2dec07196be92f926ea91b422f4abe6
Crouston <yosuam34@gmail.com>
2019-03-02 22:27:51 +0700

    Main Menu added

---
+++ Goat/Assets/Script/Button.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    public void GoToScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void OpenCanvas(GameObject canvas)
+    {
+        canvas.SetActive(true);
+    }
+
+    public void CloseCanvas(GameObject canvas)
+    {
+        canvas.SetActive(false);
+    }
+}
