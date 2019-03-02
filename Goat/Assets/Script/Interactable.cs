0cc4313f2b74963bf6703369d06af68bc77b464f
Crouston <yosuam34@gmail.com>
2019-03-02 18:09:25 +0700

    Movement added
    -player movement added
    -Interactable added

---
+++ Goat/Assets/Script/Interactable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public void BecameGoal()
+    {
+        FindObjectOfType<PlayerMovement>().goal = gameObject;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "PlayerCharacter" && collision.GetComponent<PlayerMovement>().isInteracting && collision.GetComponent<PlayerMovement>().goal == gameObject)
+        {
+            Debug.Log("Interacting");
+            collision.GetComponent<PlayerMovement>().goal = null;
+            collision.GetComponent<PlayerMovement>().isInteracting = false;
+        }
+    }
+}
---
+++ Goat/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject goal;
+    public bool isInteracting;
+    [SerializeField]
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isInteracting = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(goal != null)
+        {
+            isInteracting = true;
+            transform.position = Vector3.MoveTowards(transform.position, goal.transform.position, speed * Time.deltaTime);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            isInteracting = true;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            goal = null;
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            isInteracting = false;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            goal = null;
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            isInteracting = false;
+        }
+    }
+}
