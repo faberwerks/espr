4c3f394be52e00a2836f2d0e0a17fb702f5845ae
Adhiwira Lokacarya <adhiwira56@gmail.com>
2019-03-03 17:05:00 +0700

    Place and PlaceManager Added

---
+++ Goat/Assets/Script/Place.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Place
+{
+    public GameObject background;
+
+    public GameObject interactablePlace;
+
+    public GameObject[] arrowToOther;
+}
---
+++ Goat/Assets/Script/PlaceManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceManager : MonoBehaviour
+{
+    public GameObject[] places;
+
+    public GameObject currPlace;
+
+    public void ChangePlaces(int placeIndex)
+    {
+        currPlace.SetActive(false);
+        places[placeIndex].gameObject.SetActive(true);
+    }
+
+}
