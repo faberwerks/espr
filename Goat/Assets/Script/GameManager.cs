847aa873f1121aa832f93bfbad6b0f40a121a9fe
Crouston <yosuam34@gmail.com>
2019-03-05 03:24:54 +0700

    Background Error fixed

--- Goat/Assets/Script/GameManager.cs
+++ Goat/Assets/Script/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,8 +11,13 @@ public class GameManager : MonoBehaviour
     public float funDecrease;
     public float energyDecrease;
 
+    public float starvingCounter;
+    public float dyingCounter;
+
     private void Start()
     {
+        starvingCounter = 0;
+        dyingCounter = 0;
         hungerDecrease = 15;
         funDecrease = 15;
         energyDecrease = 15;
@@ -19,29 +25,39 @@ public class GameManager : MonoBehaviour
 
     public void ChangeCycle()
     {
+        
         FindObjectOfType<State>().UpdateState();
+        FindObjectOfType<PlayerAttribute>().FoodSpoil();
+        FindObjectOfType<PlayerAttribute>().hunger -= hungerDecrease;
+        FindObjectOfType<PlayerAttribute>().fun -= funDecrease;
+        FindObjectOfType<PlayerAttribute>().energy -= energyDecrease;
         dayCycle++;
+        CheckDead();
+        Dead();
         if (dayCycle % 2 == 0)
         {
             FindObjectOfType<ShoEvent>().StartEvent();
             foreach (GameObject place in FindObjectOfType<PlaceManager>().places)
             {
-                Place placeNow = place.GetComponent<PlaceText>().places;
-                placeNow.background.GetComponent<SpriteRenderer>().sprite = placeNow.backgroundSprite[0];
+                if (place.GetComponent<PlaceText>() != null && place.GetComponent<PlaceText>().places.background.GetComponent<SpriteRenderer>() != null)
+                {
+                    place.GetComponent<PlaceText>().places.background.GetComponent<SpriteRenderer>().sprite = place.GetComponent<PlaceText>().places.backgroundSprite[0];
+                }
+                
             }
         }
         if (dayCycle % 2 == 1)
         {
+
             foreach (GameObject place in FindObjectOfType<PlaceManager>().places)
             {
-                Place placeNow = place.GetComponent<PlaceText>().places;
-                placeNow.background.GetComponent<SpriteRenderer>().sprite = placeNow.backgroundSprite[1];
+                if (place.GetComponent<PlaceText>() != null && place.GetComponent<PlaceText>().places.background.GetComponent<SpriteRenderer>() != null)
+                {
+                    place.GetComponent<PlaceText>().places.background.GetComponent<SpriteRenderer>().sprite = place.GetComponent<PlaceText>().places.backgroundSprite[1];
+                }
             }
         }
-        FindObjectOfType<PlayerAttribute>().FoodSpoil();
-        FindObjectOfType<PlayerAttribute>().hunger -= hungerDecrease;
-        FindObjectOfType<PlayerAttribute>().fun -= funDecrease;
-        FindObjectOfType<PlayerAttribute>().energy -= energyDecrease;
+       
     }
 
     private void EraseBg()
@@ -60,6 +76,30 @@ public class GameManager : MonoBehaviour
         else
         {
             hungerDecrease = 15;
+        }
+    }
+
+    public void CheckDead()
+    {
+        if(FindObjectOfType<PlayerAttribute>().energy <= 0)
+        {
+            dyingCounter++;
+        }
+        if (FindObjectOfType<PlayerAttribute>().hunger <= 0)
+        {
+            starvingCounter++;
+        }
+    }
+
+    public void Dead()
+    {
+       if(dyingCounter >= 2)
+        {
+            SceneManager.LoadScene(0);
+        }
+        if (starvingCounter >= 2)
+        {
+            SceneManager.LoadScene(0);
         }
     }
     
